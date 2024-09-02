79cb6a69e8fb5ba6ca6190d119ff200be95e519e
Rahul Maharjan <rahulmaharjan149@gmail.com>
2024-09-02 11:58:54 +0545

    fixed image css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,22 +6,28 @@ import bgImage from "../assets/bg.jpg";
 const Home = () => {
   return (
     <>
-    <Helmet><title>Home</title></Helmet>
-    <div className="hero">
-      <div className="card bg-dark text-white border-0">
-        <img src={bgImage} className="card-img" alt="Background" height='550px' />
-        <div className="card-img-overlay d-flex flex-column justify-content-centere">
-            <div className="container">
-
-            </div>
-          <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
-          <p className="card-text lead fs-2">
-            CHECK OUT ALL THE TRENDS
-          </p>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
+      <div className="hero">
+        <div className="card bg-dark text-white border-0">
+          {/* Responsive Image */}
+          <img
+            src={bgImage}
+            className="card-img img-fluid" // Using img-fluid for responsiveness
+            alt="Background"
+            style={{ objectFit: "cover", width: "100%", height: "auto" }}
+          />
+          <div className="card-img-overlay d-flex flex-column justify-content-center">
+            <div className="container"></div>
+            <h5 className="card-title display-3 fw-bolder mb-0">
+              NEW SEASON ARRIVALS
+            </h5>
+            <p className="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+          </div>
         </div>
+        <Products />
       </div>
-      <Products/>
-    </div>
     </>
   );
 };
